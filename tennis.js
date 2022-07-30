dc6278a6904fca6276fa3f139292ff8f80602009
stevecyj <ycsteven.tsao@gmail.com>
2022-07-30 15:39:36 +0800

    thirty love

--- tennis.js
+++ tennis.js
@@ -1,19 +1,19 @@
 export class Tennis {
-    firstPlayerScoreTimes = 0
+  firstPlayerScoreTimes = 0;
 
-    scoreLookup = {
-        1: 'fifteen',
-        2: 'thirty',
-    }
+  scoreLookup = {
+    1: 'fifteen',
+    2: 'thirty',
+  };
 
-    score() {
-        if (this.firstPlayerScoreTimes === 2 || this.firstPlayerScoreTimes === 1) {
-            return `${this.scoreLookup[this.firstPlayerScoreTimes]} love`;
-        }
-        return 'love all';
+  score() {
+    if (this.firstPlayerScoreTimes === 2 || this.firstPlayerScoreTimes === 1) {
+      return `${this.scoreLookup[this.firstPlayerScoreTimes]} love`;
     }
+    return 'love all';
+  }
 
-    firstPlayerScore() {
-        this.firstPlayerScoreTimes++;
-    }
+  firstPlayerScore() {
+    this.firstPlayerScoreTimes++;
+  }
 }
--- tennis.test.js
+++ tennis.test.js
@@ -1,6 +1,6 @@
-import { Tennis } from "./tennis";
+import { Tennis } from './tennis';
 
-describe("score", function () {
+describe('score', function () {
   let tennis = new Tennis();
   beforeEach(() => {
     tennis = new Tennis();
@@ -10,26 +10,23 @@ describe("score", function () {
     expect(tennis.score()).toBe(expected);
   }
 
-  let scoreLookup = {
-    1: "fifteen",
-  };
-  it("should be love all", function () {
-    scoreShouldBe("love all");
+  it('should be love all', function () {
+    scoreShouldBe('love all');
   });
 
-  it("should be fifteen love", function () {
-    givenFirstPlayerScore(1);
-    scoreShouldBe("fifteen love");
+  it('should be fifteen love', function () {
+    givenFirstPlayerScoreTimes(1);
+    scoreShouldBe('fifteen love');
   });
 
-  function givenFirstPlayerScore(times) {
-    for (let i = 0; i < times; i++) {
+  function givenFirstPlayerScoreTimes(tiems) {
+    for (let i = 0; i < tiems; i++) {
       tennis.firstPlayerScore();
     }
   }
 
-  it("should be thirty love", function () {
-    givenFirstPlayerScore(2);
-    scoreShouldBe("thirty love");
+  it('should be thirty love', function () {
+    givenFirstPlayerScoreTimes(2);
+    scoreShouldBe('thirty love');
   });
 });
